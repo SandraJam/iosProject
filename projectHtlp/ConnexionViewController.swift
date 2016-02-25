44cd7d4ec2e671b6518fffa10162e672a16e0c5e
Moi <moi@macbook-pro-de-moi.local>
2016-02-25 18:04:58 +0100

    cacatoes

---
+++ projectHtlp/ConnexionViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ConnexionViewController.swift
+//  projectHtlp
+//
+//  Created by Moi on 25/02/2016.
+//  Copyright © 2016 projet. All rights reserved.
+//
+
+import UIKit
+
+class ConnexionViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    @IBAction func onClick(sender: AnyObject) {
+        
+        print("login: \(loginTextField.text) & password: \(passwordTextField.text)");
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ projectHtlp/InscriptionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InscriptionViewController.swift
+//  projectHtlp
+//
+//  Created by Moi on 25/02/2016.
+//  Copyright © 2016 projet. All rights reserved.
+//
+
+import UIKit
+
+class InscriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
