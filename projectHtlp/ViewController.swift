39e63f88485fc424e8c5ecc171b68c612f7780d4
tp23 <tp23@macmini2.deptinfo.univ-orleans.fr>
2016-02-25 17:03:59 +0100

    Initial Commit

---
+++ projectHtlp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectHtlp
+//
+//  Created by tp23 on 25/02/2016.
+//  Copyright © 2016 projet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
