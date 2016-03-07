d8e53214bd7dee74ae8121661cfe5f5e0788c726
tp23 <tp23@macmini2.deptinfo.univ-orleans.fr>
2016-03-07 16:14:13 +0100

    Tiger

--- projectHtlp/ViewController.swift
+++ projectHtlp/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var connexion: UIButton!
+    @IBOutlet weak var inscription: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "fond")!)
+        connexion.layer.cornerRadius = 5
+        connexion.layer.borderWidth = 2
+        connexion.layer.borderColor = UIColor.whiteColor().CGColor
+        inscription.layer.cornerRadius = 5
+        inscription.layer.borderWidth = 2
+        inscription.layer.borderColor = UIColor.whiteColor().CGColor
     }
 
     override func didReceiveMemoryWarning() {
